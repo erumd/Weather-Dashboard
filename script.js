8631b58a230cc33a4b428a17ab201bcb47c99de3
Erum Dhukka <erumdhukka531@gmail.com>
2021-01-05 13:54:31 -0600

    updated 5 day forecast. no errors but data no showing up

--- script.js
+++ script.js
@@ -152,7 +152,7 @@ console.log (APIKey);
   function getUV(lat,lon) {
     var APIKey = "76867f1d9d820e6fd45b355d5a55ddc8";
     console.log (APIKey);
-    var queryUV= "http://api.openweathermap.org/data/2.5/uvi?lat=" +lat+ "&lon=" +lon+ "&appid=" +APIKey;
+    var queryUV= "https://api.openweathermap.org/data/2.5/uvi?lat=" +lat+ "&lon=" +lon+ "&appid=" +APIKey;
 
     console.log(queryUV);
     $.ajax({
@@ -188,13 +188,12 @@ console.log (APIKey);
       function forecast (searchValue) {
       var APIKey = "76867f1d9d820e6fd45b355d5a55ddc8";
       // added lat and lon
-      lat= response.coord.lat; 
-      lon= response.coord.lon;
+      
       // var part = hourly;
       console.log ("Forecast", APIKey);
   //     // API key for 5 days
   //     // var fiveDayForecast = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=" + APIKey;
-  var oneCallAPI= "https://api.openweathermap.org/data/2.5/onecall?lat=" +lat+ "&lon=" +lon+ "&exclude=currently,minutely,hourly,alerts" +part+ "&appid=" + APIKey;
+  var oneCallAPI= "https://api.openweathermap.org/data/2.5/onecall?lat=" +lat+ "&lon=" +lon+ "&exclude=currently,minutely,hourly,alerts&appid=" + APIKey;
       // var fiveDayForecast= "http://api.openweathermap.org/data/2.5/uvi?lat=" +lat+ "&lon=" +lon+ "&appid=" +APIKey;
       console.log("This is the var fivDayForecast: ",oneCallAPI);
 
@@ -204,27 +203,29 @@ console.log (APIKey);
         method: "GET",
        }).then(function (response) {
         $(".fiveDay").each(function () {
+          lat= response.coord.lat; 
+          lon= response.coord.lon;
           // TRYING TO ADD 5 day forecast INDIVIDUALLY
 
           // response.minutely.hourly[24].temp
-        $("#icon1").attr("src", "http://openweathermap.org/img/wn/"+response.daily[0].weather.icon+"@2x.png")
+        $("#icon1").attr("src", "https://openweathermap.org/img/wn/"+response.daily[0].weather.icon+"@2x.png")
         $('#humidity1').text(`Humidity: ${response.daily[0].humidity}%`);
         // $('#humidity1').text(`Humidity: ${response.current[0].humidity}%`);
         $('#temp1').text (`<br />Temperature (F): ${Math.round ((response.daily.temp - 273.15) * 1.80 + 32)}°F`);
 
-        $("#icon2").attr("src", "http://openweathermap.org/img/wn/"+response.daily[1].weather[0].icon+"@2x.png")
+        $("#icon2").attr("src", "https://openweathermap.org/img/wn/"+response.daily[1].weather[0].icon+"@2x.png")
         $('#humidity2').text(`Humidity: ${response.daily[1].humidity}%`);
         $('#temp2').text (`<br />Temperature (F): ${Math.round ((response.daily[1].temp - 273.15) * 1.80 + 32)}°F`);
 
-        $("#icon3").attr("src", "http://openweathermap.org/img/wn/"+response.daily[2].weather[0].icon+"@2x.png");
+        $("#icon3").attr("src", "https://openweathermap.org/img/wn/"+response.daily[2].weather[0].icon+"@2x.png");
         $('#humidity3').text(`Humidity: ${response.daily[2].humidity}%`);
         $('#temp3').text (`<br />Temperature (F): ${Math.round ((response.daily[2].temp - 273.15) * 1.80 + 32)}°F`);
 
-        $("#icon4").attr("src", "http://openweathermap.org/img/wn/"+response.daily[3].weather[0].icon+"@2x.png");
+        $("#icon4").attr("src", "https://openweathermap.org/img/wn/"+response.daily[3].weather[0].icon+"@2x.png");
         $('#humidity4').text(`Humidity: ${response.daily[3].humidity}%`);
         $('#temp4').text (`<br />Temperature (F): ${Math.round ((response.daily[3].temp - 273.15) * 1.80 + 32)}°F`);
 
-        $("#icon5").attr("src", "http://openweathermap.org/img/wn/"+response.daily[4].weather[0].icon+"@2x.png");
+        $("#icon5").attr("src", "https://openweathermap.org/img/wn/"+response.daily[4].weather[0].icon+"@2x.png");
         $('#humidity5').text(`Humidity: ${response.daily[4].humidity}%`);
         $('#temp5').text (`<br />Temperature (F): ${Math.round ((response.daily[4].temp - 273.15) * 1.80 + 32)}°F`);
 
